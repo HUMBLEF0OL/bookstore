e4e11bed7e30908e6d30c88da8c9492019f01a71
Amit Rana <Amit.Rana@truminds.com>
2024-03-18 17:05:15 +0530

    created route for create book

--- frontend/src/components/Common/Header.tsx
+++ frontend/src/components/Common/Header.tsx
@@ -3,40 +3,65 @@ import React from 'react'
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import Link from 'next/link';
 
+/*
+    primary:
+*/
 const Header = () => {
     const toolbarSx = {
         backgroundColor: 'white',
         justifyContent: 'space-between',
-        color: 'black',
+        color: '#002147',
         letterSpacing: '1.25px',
         height: '60px'
 
     }
 
+    const logoButtonSx = {
+        display: 'flex',
+        flexDirection: 'column',
+        color: '#002147',
+        alignItems: 'center',
+        '&:hover': {
+            color: '#0059E6'
+        }
+    }
+
     const linkSx = {
         letterSpacing: '1.25px',
-        flexGrow: 1
+        flexGrow: 1,
+        color: '#002147',
+        '&:hover': {
+            color: '#0059E6'
+        }
     }
 
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position='static'>
                 <Toolbar sx={toolbarSx}>
-                    <Button sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <MenuBookIcon sx={{ width: '30px', height: '30px' }} />
-                        <Typography variant='subtitle1' sx={{ flexGrow: 1 }}>
-                            BookStore
-                        </Typography>
-                    </Button>
+                    <Link href={'/'} >
+                        <Box sx={logoButtonSx}>
+                            <MenuBookIcon sx={{ width: '30px', height: '30px' }} />
+                            <Typography variant='subtitle1' sx={{ flexGrow: 1 }}>
+                                BookStore
+                            </Typography>
+                        </Box>
+                    </Link>
                     <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: 2 }}>
-                        <Link href={'/about'} style={linkSx}>
-                            ABOUT
+                        <Link href={'/about'} >
+                            <Typography sx={linkSx}>
+                                ABOUT
+                            </Typography>
                         </Link>
-                        <Link href={'/create'} style={linkSx}>
-                            CREATE
+                        <Link href={'/create'}>
+                            <Typography sx={linkSx}>
+                                CREATE
+                            </Typography>
                         </Link>
-                        <Link href={'/show'} style={linkSx}>
-                            SHOW
+                        <Link href={'/show'}>
+                            <Typography sx={linkSx}>
+                                SHOW
+                            </Typography>
                         </Link>
                     </Box>
                 </Toolbar>
