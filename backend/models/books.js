d3674b77cea64d6c34df7db294911c4c4116a304
Amit Rana <Amit.Rana@truminds.com>
2024-03-18 14:09:44 +0530

    feat: api for delete,update and get books using isbn

--- backend/models/books.js
+++ backend/models/books.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    _id: mongoose.Types.ObjectId,
+    _id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        auto: true,
+        index: true
+    },
     title: {
         type: String,
         required: true
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,13 +1,18 @@
 const express = require('express')
-const { getAllBooks, getBookById, getBookByIsbn } = require('../controllers/book')
+const BookController = require('../controllers/book')
 const router = express.Router();
 
 
-router.get('/', getAllBooks)
+router.get('/', BookController.getAllBooks)
 
-router.get('/:id', getBookById)
+router.get('/:id', BookController.getBookById)
 
-router.get('/isbn/:isbn', getBookByIsbn)
+router.get('/isbn/:isbn', BookController.getBookByIsbn)
 
+router.post('/create', BookController.postBook)
+
+router.patch('/updatebook/:isbn', BookController.updateBook)
+
+router.delete('/:isbn', BookController.deleteBook)
 
 module.exports = router;
